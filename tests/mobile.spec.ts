fa9ed45f0b2f5838ab9d67acf23d580257ca0061
Ankit Kumar <ankit.kumarrrrr6055@gmail.com>
2025-11-02 12:39:53 +0530

    Test for mobiles and channel

---
+++ tests/mobile.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ...devices['iPhone 13'],
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+});
